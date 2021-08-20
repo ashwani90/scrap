1db51984efd6ea9d58b34d6f788ecb4df008e8bd
Ashwani <ashwani@90125@gmail.com>
2021-08-20 23:13:42 +0530

    Fix models

---
+++ pracs/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PracsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pracs'
---
+++ pracs/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+    def __str__(self):
+        return self.name
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return self.name
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
